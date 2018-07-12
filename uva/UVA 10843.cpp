fc9d7c9a5e87794513c4ec7713eacea9f8385895
amlosma <30425773+amlosma@users.noreply.github.com>
2018-07-13 00:57:03 +0200

    Create UVA 10843.cpp

---
+++ uva/UVA 10843.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include <stdlib.h>
+#include<assert.h>
+using namespace std;
+#define PI 3.14159265359
+#define OO 1e9
+#define S second
+#define F first
+#define MP                  make_pair
+#define sz(v) ( (int) (v.size()) )
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+#define endl "\n"
+int dx[] = { -1, 0, 1, 0 };
+int dy[] = { 0, -1, 0, 1 };
+void fast()
+{
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+int n, m;
+bool valid(int i, int j)
+{
+	return i >= 0 && i < n&&j >= 0 && j < m;
+}
+
+int main(){
+	int n; ll y, tp = 0;;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> y;
+		
+			tp = 1;
+			for (int j = 0; j < y - 2; j++)
+				tp *= y, tp %= 2000000011;
+			cout << "Case #" << i << ": " << tp << endl;
+	
+	}
+	
+}
