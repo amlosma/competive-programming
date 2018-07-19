cd32ed331d3daf86da59be466941e9c1b631380e
amlosma <30425773+amlosma@users.noreply.github.com>
2018-07-19 05:35:45 +0200

    Create UVA 264 .cpp

---
+++ uva/UVA 264 .cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#include <stdlib.h>
+#include<assert.h>
+using namespace std;
+#define PI 3.14159265359
+#define OO 1e9
+#define S second
+#define F first
+#define MP                  make_pair
+int dx[] = { 0, 0, -1, 1, 1, 1, -1, -1 };
+int dy[] = { -1, 1, 0, 0, 1, -1, 1, -1 };
+#define sz(v) ( (int) (v.size()) )
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+void fast()
+{
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+int n, m, c = 0;
+int cnt;
+bool valid(int i, int j)
+{
+	return i >= 0 && i < n&&j >= 0 && j<m;
+}
+long long  arr[1000];
+
+
+int main(){
+	
+	fast();
+	int t;
+	ll  n, m;
+	
+	while (cin>>n)
+	{
+		int i = 1, sum = 0;
+		while (1)
+		{
+			sum += i;
+			if (sum >= n)break;
+			i++;
+		}
+		cout << "TERM " << n << " IS ";
+			int w =1+ sum - n ;
+			int y = i - (sum - n);
+			if (i % 2)
+				cout << w << "/" << y << endl;
+			else
+				cout << y << "/" << w << endl;
+
+	}
+}
